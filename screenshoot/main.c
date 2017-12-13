8fc81a1a7e9cc1f37ee6ece93d8d791043061d62
Joezeo <1252795700@qq.com>
2017-12-13 23:02:07 +0800

    修正缩进🐜

--- screenshoot/main.c
+++ screenshoot/main.c
@@ -23,7 +23,7 @@ int WINAPI
 WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance, 
-    LPSTR	  lpCmdLine,
+	LPSTR	  lpCmdLine,
 	int		  nShowCmd) {
 
 	HWND			hwnd;
@@ -34,25 +34,25 @@ WinMain(
 
 	hInst = hInstance;
 
-	wndclass.style			= CS_HREDRAW | CS_VREDRAW;
+	wndclass.style				= CS_HREDRAW | CS_VREDRAW;
 
-	wndclass.lpfnWndProc	= WndProc;
+	wndclass.lpfnWndProc		= WndProc;
 
-	wndclass.cbClsExtra		= 0;
+	wndclass.cbClsExtra			= 0;
 
-	wndclass.cbWndExtra		= 0;
+	wndclass.cbWndExtra			= 0;
 
-	wndclass.hInstance		= hInstance;
+	wndclass.hInstance			= hInstance;
 
-	wndclass.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_BIGICON));
+	wndclass.hIcon				= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_BIGICON));
 
-	wndclass.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wndclass.hCursor			= LoadCursor(NULL, IDC_ARROW);
 
-	wndclass.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hbrBackground		= (HBRUSH)GetStockObject(WHITE_BRUSH);
 
-	wndclass.lpszMenuName	= "MyMenu";
+	wndclass.lpszMenuName		= "MyMenu";
 
-	wndclass.lpszClassName	= szWndClassName;
+	wndclass.lpszClassName		= szWndClassName;
 
 
 	if (!RegisterClass(&wndclass)) {
@@ -104,23 +104,23 @@ registe_sreenshoot_window(HINSTANCE hInstance) {
 
 	WNDCLASS			wndclass;
 
-	wndclass.style			= CS_HREDRAW | CS_VREDRAW;
+	wndclass.style				= CS_HREDRAW | CS_VREDRAW;
 
-	wndclass.lpfnWndProc	= ScreenProc;
+	wndclass.lpfnWndProc		= ScreenProc;
 
-	wndclass.cbClsExtra		= 0;
+	wndclass.cbClsExtra			= 0;
 
-	wndclass.cbWndExtra		= sizeof(HANDLE);
+	wndclass.cbWndExtra			= sizeof(HANDLE);
 
-	wndclass.hInstance		= hInstance;
+	wndclass.hInstance			= hInstance;
 
-	wndclass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIcon				= LoadIcon(NULL, IDI_APPLICATION);
 
-	wndclass.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wndclass.hCursor			= LoadCursor(NULL, IDC_ARROW);
 
-	wndclass.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hbrBackground		= (HBRUSH)GetStockObject(WHITE_BRUSH);
 
-	wndclass.lpszMenuName	= NULL;
+	wndclass.lpszMenuName		= NULL;
 
 	wndclass.lpszClassName	= szScreenClassName;
 
